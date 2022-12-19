93626fa2ffc23828adc173db74fba7b78d3ceec2
Mamat1411 <muhammadmujahid14@gmail.com>
2022-12-19 13:29:25 +0700

    Generating Initial UsedCar Controller and Model

---
+++ app/Http/Controllers/UsedCarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UsedCar;
+use Illuminate\Http\Request;
+
+class UsedCarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UsedCar  $usedCar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UsedCar $usedCar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UsedCar  $usedCar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UsedCar $usedCar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UsedCar  $usedCar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UsedCar $usedCar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UsedCar  $usedCar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UsedCar $usedCar)
+    {
+        //
+    }
+}
