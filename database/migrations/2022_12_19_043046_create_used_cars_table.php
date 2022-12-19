9efad93f12f42e6e7def81890348fcc24f3841a4
Mamat1411 <muhammadmujahid14@gmail.com>
2022-12-19 11:35:09 +0700

    Used Cars Table Migration

---
+++ database/migrations/2022_12_19_043046_create_used_cars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('used_cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('car_brand');
+            $table->string('car_type');
+            $table->string('car_model');
+            $table->bigInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('used_cars');
+    }
+};
