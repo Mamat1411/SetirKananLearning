6e328844d624c9aeaf651ed45baa21aee4d582e9
Mamat1411 <muhammadmujahid14@gmail.com>
2022-12-19 16:47:13 +0700

    Add Route To UsedCarController to See if It Works

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller('UsedCarController')->group(function ()
+{
+    Route::get('usedcar', 'index');
+    // Route::post('usedcar', 'store');
+    // Route::get('usedcar/{id}', 'show');
+    // Route::put('usedcar/{id}', 'update');
+    // Route::delete('usedcar/{id}', 'delete');
 });
